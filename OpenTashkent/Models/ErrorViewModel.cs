4d9c4ff72baef50b1623015a725d9358456a795b
jasur28 <yuldashevj@gmail.com>
2023-01-27 16:37:22 +0500

    Add project files.

---
+++ OpenTashkent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenTashkent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
